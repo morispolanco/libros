8ed2548fbf4a5bc406241d0806e3f1889064ab95
morispolanco <morispolanco@gmail.com>
2023-09-17 03:54:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,9 @@ else:
     openai.api_key = api_key
 
     # Función para generar texto utilizando GPT-3
-    def generar_texto(prompt):
+    def generar_texto(titulo, numero_capitulo):
+        prompt = f"Capítulo {numero_capitulo}: {titulo}\n\n"
+
         response = openai.Completion.create(
             engine='text-davinci-003',
             prompt=prompt,
@@ -22,11 +24,6 @@ else:
         )
         return response.choices[0].text.strip()
 
-    # Función para guardar el texto generado en un archivo .txt
-    def guardar_texto_en_archivo(texto_generado):
-        with open("libro_generado.txt", "w") as archivo:
-            archivo.write(texto_generado)
-
     # Función principal de la aplicación
     def main():
         st.title("Generador de Libros con GPT-3")
@@ -35,38 +32,25 @@ else:
         # Obtener el título del libro
         titulo = st.text_input("Ingresa el título del libro")
 
-        # Obtener el número de capítulos
-        num_capitulos = st.number_input("Ingresa el número de capítulos", min_value=1, step=1)
-
-        # Obtener el número de palabras por capítulo
-        num_palabras_por_capitulo = st.number_input("Ingresa el número de palabras por capítulo", min_value=1, step=1, max_value=4000)
-
-        # Obtener la audiencia a la que va dirigido el libro
-        audiencia = st.selectbox("Selecciona la audiencia a la que va dirigido el libro", ["Niños", "Adolescentes", "Adultos"])
+        # Obtener el número del capítulo
+        numero_capitulo = st.number_input("Ingresa el número del capítulo", min_value=1, step=1)
 
         # Generar el texto del libro
         if st.button("Generar"):
-            if titulo and num_capitulos and num_palabras_por_capitulo and audiencia:
-                prompt = f"#{titulo}\n\nEste libro consta de {num_capitulos} capítulos, cada uno con aproximadamente {num_palabras_por_capitulo} palabras. Está dirigido a {audiencia}.\n\n"
+            if titulo:
+                contenido_libro = generar_texto(titulo, numero_capitulo)
 
-                # Generar el contenido de cada capítulo
-                contenido_capitulos = []
-                for i in range(num_capitulos):
-                    contenido_capitulo = generar_texto(f"Capítulo {i+1}\n\n")
-                    contenido_capitulos.append(contenido_capitulo)
-
-                # Combinar el contenido de cada capítulo en el texto completo del libro
-                texto_generado = prompt + "\n\n".join(contenido_capitulos)
+                # Mostrar el texto generado
                 st.subheader("Texto generado:")
-                st.write(texto_generado)
+                st.write(contenido_libro)
 
-                # Guardar el texto generado en un archivo .txt
-                guardar_texto_en_archivo(texto_generado)
-
-                # Proporcionar un enlace de descarga al usuario
-                st.markdown("[Descargar libro generado](libro_generado.txt)")
+                # Descargar el texto generado como archivo
+                if st.button("Descargar"):
+                    with open(f"{titulo}_capitulo{numero_capitulo}.txt", "w") as file:
+                        file.write(contenido_libro)
+                    st.success("¡El archivo se ha descargado exitosamente!")
             else:
-                st.warning("Por favor, completa todos los campos.")
+                st.warning("Por favor, ingresa un título para el libro.")
 
     # Ejecutar la aplicación
     if __name__ == '__main__':
